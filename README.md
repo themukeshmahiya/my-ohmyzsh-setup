68f7e25a7da2859496bdc2ad9e7da751882197f7
Mukesh Kumar <themukeshmahiya@gmail.com>
2023-06-18 15:19:35 +0530

    version 2.0

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# my-ohmyzsh-setup-
-shell script to install oh my zsh on fedora 
+# My-ohmyzsh-setup-
+This is my shell script to install oh my zsh on fedora/centos stream/rhel/alma/rocky linux
+
+# Add a entry for github in host file to stop 443 timeout error
+1. sudo nano /etc/hosts
+2. 185.199.108.133 raw.githubusercontent.com
+3. add line 2 at the end of hosts file
+4. now execute the shell script with sudo privilege
+
+# Now add plugin to zshrc file 
+1. Run "nano.zshrc"
+2. Now replace "plugins=(git)" with "plugins=(git zsh-autosuggestions zsh-syntax-highlighting web-s   earch sudo)"
+3. Double quotes are not included.
--- zsh.sh
+++ zsh.sh
@@ -1,19 +1,15 @@
-#!/bin/sh
-#first we should install chsh to change default shell bash to zsh 
+#!/bin/bash
+#first we should install chsh-package to change default shell bash to zsh 
 sudo dnf install util-linux-user
 
 #now install zsh
 sudo dnf install zsh
 
-#now we have to solve error that cause github 443 port connection error
-#we can solve it by way but the easy solution is just edit the host file and allow it
-sudo nano /etc/hosts
-#add the line to this file at the end line is "185.199.108.133 raw.githubusercontent.com"
-
 #now install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #now we install plugins
+
 #autosuggesions plugin
 
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
@@ -22,16 +18,6 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugi
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
-#zsh-fast-syntax-highlighting plugin
-
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-
-#zsh-autocomplete plugin
-
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
-
 #now edit .zshrc
 nano .zshrc 
 
-#and replace 'plugins=(git)' with 'plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting zsh-autocomplete web-search)'
-#this is not a script now it is for my own convenience
